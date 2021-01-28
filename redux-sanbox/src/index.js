e973c7d8b5b3318206539f543966aeea94811f93
Nadya <nadya.zhidenko@bk.ru>
2021-01-28 12:23:11 +0300

    Action Creator added

--- redux-sanbox/src/index.js
+++ redux-sanbox/src/index.js
@@ -8,30 +8,41 @@ const reducer = (state = 0, action) => {
     switch(action.type){
         case "INC":
             return state+1;
+        case "RND":
+            return state+action.payload;
         case "DEC":
-            return state-1;
+            return state -1;
         default:
             return state;
     } 
 };
 
 const store = createStore(reducer);
+
+const inc = () =>  ({type: 'INC'});
+const dec = () =>  ({type: 'DEC'});
+const rnd = (payload) => ({type: 'RND', payload});
+
 document
     .getElementById('inc')
     .addEventListener('click', () => {
-        store.dispatch({type:'INC'});
+        store.dispatch(inc());
     });
 document
     .getElementById('dec')
     .addEventListener('click', () => {
-        store.dispatch({type:'DEC'});
+        store.dispatch(dec());
+    });
+document
+    .getElementById('rnd')
+    .addEventListener('click', () => {
+        const payload = Math.floor(Math.random()*10);
+        store.dispatch(rnd(payload));
     });
 const update = () => {
     document
         .getElementById('counter')
         .innerHTML = store.getState();
 };
-
-
 
 store.subscribe(update);
